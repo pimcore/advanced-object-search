abc78a7ac27b1aa8f5a40622ea41db95e81391c2
cfasching <christian.fasching@elements.at>
2017-08-11 15:53:57 +0200

    added quantity value

--- lib/ESBackendSearch/FieldDefinitionAdapter/QuantityValue.php
+++ lib/ESBackendSearch/FieldDefinitionAdapter/QuantityValue.php
@@ -6,20 +6,22 @@ use DeepCopy\Filter\Filter;
 use ESBackendSearch\FieldSelectionInformation;
 use ESBackendSearch\FilterEntry;
 use ONGR\ElasticsearchDSL\BuilderInterface;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
 use ONGR\ElasticsearchDSL\Query\RangeQuery;
 use ONGR\ElasticsearchDSL\Query\TermQuery;
 use Pimcore\Model\Object\AbstractObject;
 use Pimcore\Model\Object\ClassDefinition\Data;
 use Pimcore\Model\Object\Concrete;
+use function Sodium\add;
 
-class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
+class QuantityValue extends Numeric implements IFieldDefinitionAdapter {
 
     /**
      * field type for search frontend
      *
      * @var string
      */
-    protected $fieldType = "numeric";
+    protected $fieldType = "quantityValue";
 
     /**
      * @return array
@@ -31,10 +33,24 @@ class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
                 [
                     'properties' => [
                         self::ES_MAPPING_PROPERTY_STANDARD => [
-                            'type' => 'float',
+                            'properties' => [
+                                'value' => [
+                                    'type' => 'float',
+                                ],
+                                'unit' => [
+                                    'type' => 'integer',
+                                ]
+                            ]
                         ],
                         self::ES_MAPPING_PROPERTY_NOT_INHERITED => [
-                            'type' => 'float',
+                            'properties' => [
+                                'value' => [
+                                    'type' => 'float',
+                                ],
+                                'unit' => [
+                                    'type' => 'integer',
+                                ]
+                            ]
                         ]
                     ]
                 ]
@@ -43,7 +59,15 @@ class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
             return [
                 $this->fieldDefinition->getName(),
                 [
-                    'type' => 'float',
+                    'properties' => [
+                        'value' => [
+                            'type' => 'float',
+                        ],
+                        'unit' => [
+                            'type' => 'integer',
+                        ]
+                    ]
+
                 ]
             ];
         }
@@ -54,24 +78,25 @@ class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
      * @param $fieldFilter
      *
      * filter field format as follows:
-     *   - simple number like
-     *       234.54   --> creates TermQuery
+     *   - simple array with number/unitID like
+     *       ["value" => 234.54, "unit" => 3]   --> creates TermQuery
      *   - array with gt, gte, lt, lte like
-     *      ["gte" => 40, "lte" => 45] --> creates RangeQuery
+     *      ["value" => ["gte" => 40, "lte" => 45], "unit" => 3] --> creates RangeQuery
      *
      * @param bool $ignoreInheritance
      * @param string $path
      * @return BuilderInterface
      */
     public function getQueryPart($fieldFilter, $ignoreInheritance = false, $path = "") {
-        if(is_array($fieldFilter)) {
-            return new RangeQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance), $fieldFilter);
+        $boolQuery = new BoolQuery();
+        if(is_array($fieldFilter) && is_array($fieldFilter['value'])) {
+            $boolQuery->add(new RangeQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance) . ".value", $fieldFilter["value"]));
         } else {
-            return new TermQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance), $fieldFilter);
+            $boolQuery->add(new TermQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance) . ".value", $fieldFilter["value"]));
         }
+        $boolQuery->add(new TermQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance) . ".unit", $fieldFilter["unit"]));
+        return $boolQuery;
     }
-
-
 
     /**
      * returns selectable fields with their type information for search frontend
@@ -86,7 +111,8 @@ class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
             $this->fieldType,
             [
                 'operators' => ['lt', 'lte', 'eq', 'gte', 'gt', FilterEntry::EXISTS, FilterEntry::NOT_EXISTS ],
-                'classInheritanceEnabled' => $this->considerInheritance
+                'classInheritanceEnabled' => $this->considerInheritance,
+                'units' => $this->fieldDefinition->getValidUnits()
             ]
         )];
     }
@@ -103,6 +129,8 @@ class Numeric extends DefaultAdapter implements IFieldDefinitionAdapter {
         }
 
         $value = $this->fieldDefinition->getForWebserviceExport($object);
+        unset($value['unitAbbreviation']);
+
 
         if($ignoreInheritance) {
             AbstractObject::setGetInheritedValues($inheritanceBackup);
