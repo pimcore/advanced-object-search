512a338892dffbcfb9e7e676c40406c86935a59b
cfasching <christian.fasching@elements.at>
2017-08-11 11:39:31 +0200

    added select

---
+++ lib/ESBackendSearch/FieldDefinitionAdapter/Select.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace ESBackendSearch\FieldDefinitionAdapter;
+
+use DeepCopy\Filter\Filter;
+use ESBackendSearch\FieldSelectionInformation;
+use ESBackendSearch\FilterEntry;
+use ONGR\ElasticsearchDSL\BuilderInterface;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\RangeQuery;
+use ONGR\ElasticsearchDSL\Query\TermQuery;
+use Pimcore\Model\Object\AbstractObject;
+use Pimcore\Model\Object\ClassDefinition\Data;
+use Pimcore\Model\Object\Concrete;
+
+class Select extends DefaultAdapter implements IFieldDefinitionAdapter {
+
+    /**
+     * field type for search frontend
+     *
+     * @var string
+     */
+    protected $fieldType = "select";
+
+    /**
+     * @return array
+     */
+    public function getESMapping() {
+        if($this->considerInheritance) {
+            return [
+                $this->fieldDefinition->getName(),
+                [
+                    'properties' => [
+                        self::ES_MAPPING_PROPERTY_STANDARD => [
+                            'type' => 'string',
+                            'index' => 'not_analyzed'
+                        ],
+                        self::ES_MAPPING_PROPERTY_NOT_INHERITED => [
+                            'type' => 'string',
+                            'index' => 'not_analyzed'
+                        ]
+                    ]
+                ]
+            ];
+        } else {
+            return [
+                $this->fieldDefinition->getName(),
+                [
+                    'type' => 'string',
+                    'index' => 'not_analyzed'
+                ]
+            ];
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldSelectionInformation()
+    {
+        return [new FieldSelectionInformation(
+            $this->fieldDefinition->getName(),
+            $this->fieldDefinition->getTitle(),
+            $this->fieldType,
+            [
+                'operators' => [BoolQuery::MUST, BoolQuery::SHOULD, BoolQuery::MUST_NOT, FilterEntry::EXISTS, FilterEntry::NOT_EXISTS],
+                'classInheritanceEnabled' => $this->considerInheritance,
+                'options' => $this->fieldDefinition->getOptions()
+            ]
+        )];
+    }
+
+}
