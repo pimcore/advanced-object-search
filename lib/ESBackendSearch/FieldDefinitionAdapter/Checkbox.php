f6738c610cb4abd7d38545dc25ac760ec9484462
cfasching <christian.fasching@elements.at>
2017-08-11 11:22:30 +0200

    added checkbox

---
+++ lib/ESBackendSearch/FieldDefinitionAdapter/Checkbox.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace ESBackendSearch\FieldDefinitionAdapter;
+
+use ESBackendSearch\FieldSelectionInformation;
+use ESBackendSearch\FilterEntry;
+use ESBackendSearch\Service;
+use ONGR\ElasticsearchDSL\BuilderInterface;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\ExistsQuery;
+use ONGR\ElasticsearchDSL\Query\QueryStringQuery;
+use ONGR\ElasticsearchDSL\Query\TermQuery;
+use Pimcore\Model\Object\AbstractObject;
+use Pimcore\Model\Object\ClassDefinition;
+use Pimcore\Model\Object\ClassDefinition\Data;
+use Pimcore\Model\Object\Concrete;
+
+class Checkbox extends DefaultAdapter implements IFieldDefinitionAdapter {
+
+    /**
+     * field type for search frontend
+     *
+     * @var string
+     */
+    protected $fieldType = "checkbox";
+
+
+    /**
+     * @return array
+     */
+    public function getESMapping() {
+
+        if($this->considerInheritance) {
+            return [
+                $this->fieldDefinition->getName(),
+                [
+                    'properties' => [
+                        self::ES_MAPPING_PROPERTY_STANDARD => [
+                            'type' => 'boolean',
+                        ],
+                        self::ES_MAPPING_PROPERTY_NOT_INHERITED => [
+                            'type' => 'boolean',
+                        ]
+                    ]
+                ]
+            ];
+        } else {
+            return [
+                $this->fieldDefinition->getName(),
+                [
+                    'type' => 'boolean',
+                ]
+            ];
+        }
+    }
+
+    /**
+     * @param Concrete $object
+     * @param bool $ignoreInheritance
+     */
+    protected function doGetIndexDataValue($object, $ignoreInheritance = false) {
+        $inheritanceBackup = null;
+        if($ignoreInheritance) {
+            $inheritanceBackup = AbstractObject::getGetInheritedValues();
+            AbstractObject::setGetInheritedValues(false);
+        }
+
+        $value = $this->fieldDefinition->getForWebserviceExport($object);
+
+        if($ignoreInheritance) {
+            AbstractObject::setGetInheritedValues($inheritanceBackup);
+        }
+
+        return (bool) $value;
+    }
+
+
+    /**
+     * @param Concrete $object
+     * @return mixed
+     */
+    public function getIndexData($object) {
+
+        $value = $this->doGetIndexDataValue($object, false);
+
+        if($this->considerInheritance) {
+            $notInheritedValue = $this->doGetIndexDataValue($object, true);
+
+            $returnValue = [];
+            $returnValue[self::ES_MAPPING_PROPERTY_STANDARD] = $value;
+            $returnValue[self::ES_MAPPING_PROPERTY_NOT_INHERITED] = $notInheritedValue;
+
+            return $returnValue;
+        } else {
+            return $value;
+        }
+    }
+
+    /**
+     * @param $fieldFilter
+     *
+     * filter field format as follows:
+     *   - simple boolean like
+     *       true | false  --> creates QueryStringQuery
+     *
+     * @param bool $ignoreInheritance
+     * @param string $path
+     * @return BuilderInterface
+     */
+    public function getQueryPart($fieldFilter, $ignoreInheritance = false, $path = "") {
+        return new TermQuery($path . $this->fieldDefinition->getName() . $this->buildQueryFieldPostfix($ignoreInheritance), $fieldFilter);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldSelectionInformation()
+    {
+        return [new FieldSelectionInformation(
+            $this->fieldDefinition->getName(),
+            $this->fieldDefinition->getTitle(),
+            $this->fieldType,
+            [
+                'operators' => [BoolQuery::MUST, BoolQuery::MUST_NOT],
+                'classInheritanceEnabled' => $this->considerInheritance
+            ]
+        )];
+    }
+}
