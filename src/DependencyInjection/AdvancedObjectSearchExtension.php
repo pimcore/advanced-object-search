eab6380211ec87eb398e61c974a33d07317ba477
Christian Fasching <christian.fasching@elements.at>
2017-08-14 15:16:44 +0200

    [pimcore5] added bundle extension

---
+++ src/DependencyInjection/AdvancedObjectSearchExtension.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace AdvancedObjectSearchBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AdvancedObjectSearchExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+
+        $loader->load('services.yml');
+    }
+}
