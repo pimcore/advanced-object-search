69cb731cee33c348b9d737dc73cbb7ef9c89a8ca
Christian Fasching <christian.fasching@elements.at>
2017-08-14 13:49:51 +0200

    [pimcore5] updated license header

--- src/Resources/public/js/searchConfig/conditionPanelContainerBuilder.js
+++ src/Resources/public/js/searchConfig/conditionPanelContainerBuilder.js
@@ -1,3 +1,17 @@
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+
 pimcore.registerNS("pimcore.plugin.esbackendsearch.searchConfig.conditionPanelContainerBuilder");
 pimcore.plugin.esbackendsearch.searchConfig.conditionPanelContainerBuilder = Class.create({
 
