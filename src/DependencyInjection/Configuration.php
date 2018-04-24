bab08b417f392668c83063ffb28085e6cb0b399c
Christian Fasching <christian.fasching@elements.at>
2018-04-24 17:30:40 +0200

    implemented mapping configuration for field definition adapters. fixes #9

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace AdvancedObjectSearchBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('pimcore_advanced_object_search');
+
+        $rootNode
+            ->children()
+                ->arrayNode('field_definition_adapters')
+                    ->info("Add mapping between data object type and service implementation for field definition adapter")
+                    ->useAttributeAsKey('name')
+                        ->prototype('scalar')
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
