32133b57328e027c8e5168db3d07b6aee413d18a
Jacob Dreesen <jacob@hdreesen.de>
2020-04-28 10:28:36 +0200

    Register the maintenance job for update queue processing in Pimcore 6 (#86)
    * Add new maintenance task for Pimcore v6 support
    
    * Move legacy job registration logic to new class

---
+++ src/Maintenance/UpdateQueueProcessor.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+
+namespace AdvancedObjectSearchBundle\Maintenance;
+
+
+use AdvancedObjectSearchBundle\Service;
+use Pimcore\Event\System\MaintenanceEvent;
+use Pimcore\Maintenance\TaskInterface;
+use Pimcore\Model\Schedule\Maintenance\Job;
+
+class UpdateQueueProcessor implements TaskInterface
+{
+    /**
+     * @var Service
+     */
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Note: When Pimcore v5 support is dropped, this can be removed.
+     */
+    public function registerMaintenanceJob(MaintenanceEvent $maintenanceEvent)
+    {
+        $maintenanceEvent->getManager()->registerJob(new Job(get_class($this), [$this, "execute"]));
+    }
+
+    public function execute()
+    {
+        $this->service->processUpdateQueue(500);
+    }
+}
