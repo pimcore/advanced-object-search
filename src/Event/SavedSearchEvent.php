16194762c92216db6f6453cc2b27364ba03fda3c
Vladimir Malik <vladimir@convert.no>
2021-01-21 11:21:39 +0300

    Added events for adding event listener on save or delete saved search.

---
+++ src/Event/SavedSearchEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AdvancedObjectSearchBundle\Event;
+
+use AdvancedObjectSearchBundle\Model\SavedSearch;
+use Symfony\Component\EventDispatcher\Event;
+
+class SavedSearchEvent extends Event
+{
+    /**
+     * @var SavedSearch
+     */
+    protected $search;
+
+    public function __construct(SavedSearch $search)
+    {
+        $this->search = $search;
+    }
+
+    /**
+     * @return Search
+     */
+    public function getSavedSearch(): SavedSearch
+    {
+        return $this->search;
+    }
+}
