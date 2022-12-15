f97fdfd851ff98b7fb0ad1f32e6db3f64f857b8a
dvesh3 <dvesh3@users.noreply.github.com>
2022-12-15 15:01:23 +0000

    Apply php-cs-fixer changes

--- src/Migrations/PimcoreX/Version20221130130306.php
+++ src/Migrations/PimcoreX/Version20221130130306.php
@@ -2,10 +2,23 @@
 
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace AdvancedObjectSearchBundle\Migrations\PimcoreX;
 
-use Doctrine\DBAL\Schema\Schema;
 use AdvancedObjectSearchBundle\Installer;
+use Doctrine\DBAL\Schema\Schema;
 use Pimcore\Migrations\BundleAwareMigration;
 
 class Version20221130130306 extends BundleAwareMigration
