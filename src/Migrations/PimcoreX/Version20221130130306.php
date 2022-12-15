acb61dd9a2b7065785122c2293390249e2530a04
dvesh3 <divesh.pahuja@pimcore.com>
2022-12-15 16:00:56 +0100

    follow up to #194
    Signed-off-by: dvesh3 <divesh.pahuja@pimcore.com>

--- src/Migrations/PimcoreX/Version20221130130306.php
+++ src/Migrations/PimcoreX/Version20221130130306.php
@@ -2,26 +2,24 @@
 
 declare(strict_types=1);
 
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
- */
-
 namespace AdvancedObjectSearchBundle\Migrations\PimcoreX;
 
-use AdvancedObjectSearchBundle\Installer;
 use Doctrine\DBAL\Schema\Schema;
+use AdvancedObjectSearchBundle\Installer;
+use Pimcore\Migrations\BundleAwareMigration;
 
-class Version20221130130306 extends AbstractPimcoreMigration
+class Version20221130130306 extends BundleAwareMigration
 {
+    protected function getBundleName(): string
+    {
+        return 'AdvancedObjectSearchBundle';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Migrate `o_id` column to `id` in queue table';
+    }
+
     public function up(Schema $schema): void
     {
         $table = $schema->getTable(Installer::QUEUE_TABLE_NAME)->getName();
