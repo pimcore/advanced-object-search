f4b8810222f5b3bab3b6629bec185e2b8ffe0961
JiaJia Ji <kingjia90@gmail.com>
2023-01-13 18:38:33 +0100

    [Task]: Fix void return type (#195)
    * Task: add native return type void

--- src/Maintenance/UpdateQueueProcessor.php
+++ src/Maintenance/UpdateQueueProcessor.php
@@ -48,7 +48,7 @@ class UpdateQueueProcessor implements TaskInterface
         $this->queueHandler = $queueHandler;
     }
 
-    public function execute()
+    public function execute(): void
     {
         if ($this->messengerQueueActivated) {
             $this->queueHandler->dispatchMessages();
