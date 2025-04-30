61adbfc9c7419a84fcd0f4803fe0df94fb0ce043
Christian Fasching <fashxp@users.noreply.github.com>
2025-04-30 10:07:31 +0200

    License Switch to POCL (#296)
    * License Switch to POCL
    
    * License Switch to POCL
    
    * fix php-style fixer

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,16 +2,13 @@
 declare(strict_types=1);
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace AdvancedObjectSearchBundle\Controller;
@@ -54,11 +51,13 @@ class AdminController extends UserAwareController
                 $classId = strip_tags($request->query->getString('class_id'));
                 $definition = DataObject\ClassDefinition::getById($classId);
                 $allowInheritance = $definition->getAllowInherit();
+
                 break;
 
             case 'fieldcollection':
                 $key = strip_tags($request->query->getString('key'));
                 $definition = DataObject\Fieldcollection\Definition::getByKey($key);
+
                 break;
 
             case 'objectbrick':
--- src/Model/SavedSearch/Dao.php
+++ src/Model/SavedSearch/Dao.php
@@ -2,16 +2,13 @@
 declare(strict_types=1);
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace AdvancedObjectSearchBundle\Model\SavedSearch;
@@ -49,6 +46,7 @@ class Dao extends Model\Dao\AbstractDao
     public function save()
     {
         $this->db->beginTransaction();
+
         try {
             $dataAttributes = get_object_vars($this->model);
 
@@ -75,6 +73,7 @@ class Dao extends Model\Dao\AbstractDao
             return true;
         } catch (\Exception $e) {
             $this->db->rollBack();
+
             throw $e;
         }
     }
@@ -87,12 +86,14 @@ class Dao extends Model\Dao\AbstractDao
     public function delete()
     {
         $this->db->beginTransaction();
+
         try {
             $this->db->delete(self::TABLE_NAME, ['id' => $this->model->getId()]);
 
             $this->db->commit();
         } catch (\Exception $e) {
             $this->db->rollBack();
+
             throw $e;
         }
     }
